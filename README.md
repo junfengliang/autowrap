9f808f872fbe34ce3627112cfc9419854f63869e
junfeng <18552208@qq.com>
2021-12-26 17:04:42 +0800

    Auto wrap init

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# autowrap
-Autowrap is an open source javascript module to handle text wrap. It support both chinese and english.
+# AutoWrap
+AutoWrap is an open source javascript module to handle text wrap. It support both chinese and english.
+
+## example
+
+```
+    import AutoWrap from 'autowrap'
+    var AutoWrap = new AutoWrap();
+    var text = "AutoWrap is an open source javascript module to handle text wrap. 中文也是支持的: 床前明月光，疑是地上霜，举头望明月，低头思故乡。long word wrap is also supported: abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz12345678901234567890";
+    console.log(AutoWrap.wrap(text,200,"14px Arial").join("\n"))
+```
+
+output
+
+```
+AutoWrap is an open source
+javascript module to handle
+text wrap. 中文也是支持的: 床
+前明月光，疑是地上霜，举头望
+明月，低头思故乡。long word
+wrap is also supported: abcdef
+ghijklmnopqrstuvwxyz12345678
+90abcdefghijklmnopqrstuvwxyz
+12345678901234567890
+```
+If you change the call to
+
+```
+    console.log(AutoWrap.wrap(text,50).join("\n"))
+```
+
+It will output
+
+```
+AutoWrap is an open source javascript module to
+handle text wrap. 中文也是支持的: 床前明月光，疑
+是地上霜，举头望明月，低头思故乡。long word wrap
+is also supported: abcdefghijklmnopqrstuvwxyz1234
+567890abcdefghijklmnopqrstuvwxyz123456789012345678
+90
+```
+
---
+++ index.js
@@ -0,0 +1,96 @@
+
+var AutoWrap;
+
+(function(){
+    var ctx = null;
+    function ctxWidth(text,font){
+        if(ctx==null){
+            ctx = document.createElement("canvas").getContext("2d");
+        }
+        ctx.font = font;
+        return ctx.measureText(text).width;
+    }
+    function simpleWidth(text,font){
+        let cur =0;            
+        let width = 0;
+        while(cur<text.length){  
+            if(text.charCodeAt(cur++)>255){
+                width+=2
+            }else{
+                width+=1
+            }            
+        }
+        
+        return width;
+    }
+
+    function autowrap(text,font,maxWidth,widthFunc){
+        var arr = text.split("\n");
+        var lines = [];
+        for (var i = 0; i < arr.length; i++) {
+            var line = arr[i];
+            var lineWidth = widthFunc(line,font);
+            if(lineWidth<=maxWidth){
+                lines.push(line);
+                continue;
+            }
+            let cur =0; 
+            var tmpLine = "";
+
+            while(cur<line.length){                
+                let word = "" + text[cur++];
+                while(cur < text.length && text[cur]!=' ' && text.charCodeAt(cur)<256) {                        
+                    word += text[cur++];
+                }
+                var wordWidth = widthFunc(word,font);
+                let tempLineWidth = widthFunc(tmpLine,font);
+
+                if(wordWidth>maxWidth){
+                    let wordCur =0;
+                    let tmpWord = word[wordCur++];
+                    while(wordCur<word.length){
+                        let tmpWidth = widthFunc(tmpWord + word[wordCur],font);
+                        if(tmpWidth+tempLineWidth>maxWidth){
+                            lines.push((tmpLine+tmpWord).trim());
+                            tmpLine = "";
+                            tempLineWidth = 0;
+                            tmpWord=word[wordCur];
+                        }else{
+                            tmpWord += word[wordCur];
+                        }
+                        wordCur++
+                    }
+                    tmpLine = tmpWord
+                }else{
+                    if(wordWidth+tempLineWidth>maxWidth){
+                        lines.push(tmpLine.trim())
+                        if(word[0]!=' ' || word.length>1){
+                            tmpLine = word;
+                        }else{
+                            tmpLine = "";
+                        }                        
+                    }else{
+                        tmpLine +=word;
+                    }
+                }
+            }
+            if(tmpLine.length>0){
+                lines.push(tmpLine.trim())
+            }
+        }
+        return lines;
+    }
+    AutoWrap = function(){
+    }
+    
+    AutoWrap.prototype.wrap = function(text,maxWidth,font){
+        if(font==undefined){
+            return autowrap(text,"",maxWidth,simpleWidth)
+        }
+        return autowrap(text,font,maxWidth,ctxWidth)
+    }
+})()
+if (typeof module !== 'undefined' && typeof exports === 'object') {
+    module.exports = AutoWrap;
+}
+
